9d5b4b75f15d5388ab991bb5ebaba37839379b68
Andrew Neth <andrew.neth@xplortechnologies.com>
2025-08-19 11:28:43 -0500

    attempt to add auto-overrides v1.1

---
+++ auto-overrides.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        Automatic override toggle
+// @namespace   https://github.com/attn-xplor/userscripts
+// @match       https://*.clearent.net/*
+// @grant       none
+// @version     1.1
+// @author      Andrew Neth
+// ==/UserScript==
+
+const imo = window.importMapOverrides;
+
+async function checkMfe([mfe, url]) {
+  const isEnabled = !imo.isDisabled(mfe);
+
+  let isRunning;
+  try {
+    await fetch(url);
+    isRunning = true;
+  } catch (e) {
+    isRunning = false;
+  }
+
+  if (isRunning != isEnabled) {
+    isRunning ? imo.enableOverride(mfe) : imo.disableOverride(mfe);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function main() {
+  const importMap = imo.getOverrideMap(true);
+  const promises = Object.entries(importMap.imports).map(checkMfe);
+  const outcomes = await Promise.allSettled(promises);
+  if (outcomes.some(o => o.value)) {
+    location.reload();
+  }
+}
+
+if (!!imo) {
+  main();
+}

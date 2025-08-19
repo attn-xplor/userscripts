f1a15f4fa6984904d7731799f9f8d5cdf04bc495
ilopez <ismael.lopez@xplortechnologies.com>
2025-08-19 11:59:21 -0500

    feat: add okta utils

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # [Automatic override toggle](//github.com/attn-xplor/userscripts/raw/refs/heads/trunk/auto-overrides.user.js)
 # [Config Patch](//github.com/attn-xplor/userscripts/raw/refs/heads/trunk/config-patch.user.js)
+# [Okta Utils](//github.com/attn-xplor/userscripts/raw/refs/heads/trunk/okta-utils.user.js)
---
+++ okta-utils.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Okta Token
+// @match       https://my*.clearent.net/ui/*
+// @match       http://localhost:4200/*
+// @grant       GM.registerMenuCommand
+// @grant       GM.setClipboard
+// @author      Ismael Lopez
+// @version     1.0
+// ==/UserScript==
+
+GM.registerMenuCommand('Copy Token', () => {
+  const key = (window.location.hostname === 'my.clearent.net')
+    ? 'oidc.user:https://auth.clearent.net/oauth2/aus4ulyubshD7M0yf697:0oa6ggt30dFSxSVxX697'
+    : 'oidc.user:https://auth-sb.clearent.net/oauth2/aus3a1kavt9qzEcsz1d7:0oa3a1ic7mGSRLqrZ1d7';
+  const value = sessionStorage.getItem(key);
+  if (!value) {
+    alert(`Key '${key}' not found in session storage`);
+    return;
+  }
+  GM.setClipboard(JSON.parse(value).access_token);
+});
